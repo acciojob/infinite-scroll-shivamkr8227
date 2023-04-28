fd9df2ad55ea9f6f39283eb603561f120d479540
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-28 11:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here!
-
+let olid = document.getElementById("infi-list");
+for (let i = 1; i <= 10; i++) {
+  let liele = document.createElement("li");
+  liele.textContent = "Item " + i;
+  olid.appendChild(liele);
+}
+let i=11;
+olid.addEventListener("scroll",function(){
+	for( let j=0 ; j<2 ; j++){
+		let liele = document.createElement("li");
+		liele.textContent = "Item " + i;
+		i++;
+		olid.appendChild(liele);
+	}
+});
